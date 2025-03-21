d2bc35e5d15b7596e07d6f0b5d94ff5864d5d26d
Vincent Zhang <vincent.zhang@ualberta.ca>
2025-03-21 16:05:02 -0600

    update physical params

--- mujoco_playground/_src/locomotion/hal/randomize.py
+++ mujoco_playground/_src/locomotion/hal/randomize.py
@@ -58,9 +58,9 @@ def domain_randomize(model: mjx.Model, rng: jax.Array):
     )
     body_mass = model.body_mass.at[:].set(model.body_mass * dmass)
 
-    # Add mass to torso: +U(-1.0, 1.0).
+    # Add mass to torso: +U(-0.2, 0.2).
     rng, key = jax.random.split(rng)
-    dmass = jax.random.uniform(key, minval=-1.0, maxval=1.0)
+    dmass = jax.random.uniform(key, minval=-0.2, maxval=0.2)
     body_mass = body_mass.at[TORSO_BODY_ID].set(
         body_mass[TORSO_BODY_ID] + dmass
     )
